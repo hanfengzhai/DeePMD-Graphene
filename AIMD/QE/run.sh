c7bb7ffdad166d019b6c5d4c9a085fc63307677a
Hanfeng Zhai <frankzhai0@gmail.com>
2022-06-26 19:14:42 -0400

    Update run.sh

--- AIMD/QE/run.sh
+++ AIMD/QE/run.sh
@@ -1,4 +1,13 @@
-# use these two commands to run this task
-# add your mpirun commands based on your machine configuration
+#!/bin/sh
+#SBATCH -J dmd
+#SBATCH -p compute
+#SBATCH -o deepmd%j.out    
+#SBATCH -N 1
+#SBATCH -n 60
+#SBATCH -t 20:00:00
+#SBATCH -A TG-BIO210063       # Project/Allocation name (req'd if you have more than 1)
+#SBATCH --mail-user=hz253@cornell.edu
+
+
 cp.x < graphene.gs.in > graphene.gs.out
 cp.x < graphene.in > graphene
